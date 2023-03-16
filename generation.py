ffb7fac154e21a19afbdedcb59a6fd88b1176f79
adrianko <adrian.kordas@nask.pl>
2023-03-16 10:17:30 +0100

    Generation

---
+++ generation.py
@@ -0,0 +1,77 @@
+import os
+import uuid
+from PIL import Image
+import wget
+
+#python .\stylegan2\generate.py --outdir=out --trunc=0.7 --seeds=600 --network=.\stylegan2_models\StyleGAN2_microscopev1.pkl    <---- Zdjęcia pod mikroskopem
+#python .\stylegan2\generate.py --outdir=out --trunc=0.7 --seeds=600 --network=.\stylegan2_models\Long_Krrrl_Drawings__final.pkl    <---- Rysunki
+#python .\stylegan2\generate.py --outdir=out --trunc=0.7 --seeds=600 --network=.\stylegan2_models\mapdreamer.pkl   <---- Mapy
+#python .\stylegan2\generate.py --outdir=out --trunc=0.7 --seeds=123 --network=.\stylegan2_models\network-snapshot-027750.pkl   <---- Ptaki
+#python .\stylegan2\generate.py --outdir=out --trunc=0.7 --seeds=123 --network=.\stylegan2_models\stylegan2-horse-config-f.pkl   <---- Konie
+#python .\stylegan2\generate.py --outdir=out --trunc=0.7 --seeds=123 --network=.\stylegan_models\stylegan2-ffhq-512x512  <---- Twarze
+#python .\stylegan2\generate.py --outdir=out --trunc=0.7 --seeds=123 --network=.\stylegan2_models\stylegan2-church-config-f.pkl   <---- Kościoły
+#python .\stylegan2\generate.py --outdir=out --trunc=0.7 --seeds=123 --network=.\stylegan2_models\stylegan2-car-config-f.pkl   <---- Samochody
+#python .\stylegan2\generate.py --outdir=out --trunc=0.7 --seeds=123 --network=.\stylegan2_models\stylegan2-afhqcat-512x512.pkl   <---- Koty
+#python .\stylegan2\generate.py --outdir=out --trunc=0.7 --seeds=123 --network=.\stylegan2_models\stylegan2-afhqdog-512x512.pkl  <---- Psy
+#python .\stylegan2\generate.py --outdir=out --trunc=0.7 --seeds=123 --network=.\stylegan2_models\stylegan2-afhqwild-512x512.pkl  <---- Zwierzęta
+
+
+
+class Generation:
+    def __init__(self, target_model, target_seed, target_trunc):
+        self.model = None
+        if target_model == "CAT":
+            self.model = 'stylegan2-afhqcat-512x512.pkl'
+
+        if target_model == "DOG":
+            self.model = 'stylegan2-afhqdog-512x512.pkl'
+
+        if target_model == "QV2":
+            self.model = 'stylegan2-ffhq-512x512.pkl'
+
+        if target_model == "WILD":
+            self.model = 'stylegan2-afhqwild-512x512.pkl'
+
+        if target_model == "BRECAHAD":
+            self.model = 'stylegan2-brecahad-512x512.pkl'
+
+        if target_model == "CELEBAHQ":
+            self.model = 'stylegan2-celebahq-256x256.pkl'
+
+        if target_model == "METAFACES":
+            self.model = 'stylegan2-metfaces-1024x1024.pkl'
+        if not self.file_exists(f'.\stylegan2_models\{self.model}'):
+            #Download model via wget
+            self.download_model(self.model)
+
+        self.target_seed = target_seed
+        self.target_trunc = target_trunc
+    
+    def generate(self):
+        #random directory name
+        random_dir_name = str(uuid.uuid4())
+        if self.target_seed == -1:
+            #Generate random seed
+            self.target_seed = str(uuid.uuid4().int)[:6]
+        os.system(f'python .\stylegan2\generate.py --outdir=.\out\{random_dir_name} --trunc={self.target_trunc} --seeds={self.target_seed} --network=.\stylegan2_models\{self.model}')
+        #Wait for image to be generated
+        while not os.path.exists(f'.\out\{random_dir_name}'):
+            pass
+        for image_name in os.listdir(f'.\out\{random_dir_name}'):
+            #Open as PIL image
+            image = Image.open(f'.\out\{random_dir_name}\{image_name}')
+            return image
+        
+    def file_exists(self, path):
+        if os.path.isfile(path):
+            return True
+        else:
+            return False
+        
+    def download_model(self, name):
+        print(f'https://api.ngc.nvidia.com/v2/models/nvidia/research/stylegan2/versions/1/files/{name}')
+        wget.download(f'https://api.ngc.nvidia.com/v2/models/nvidia/research/stylegan2/versions/1/files/{name}', f'.\stylegan2_models\{name}')
+
+        #https://api.ngc.nvidia.com/v2/models/nvidia/research/stylegan2/versions/1/files/stylegan2-afhqcat-512x512.pkl
+        #https://api.ngc.nvidia.com/v2/models/nvidia/research/stylegan2/versions/1/files/stylegan2_afhqcat_512x512.pkl
+        #https://api.ngc.nvidia.com/v2/models/nvidia/research/stylegan2/versions/1/files/stylegan2-afhqcat-512x512.pkl
